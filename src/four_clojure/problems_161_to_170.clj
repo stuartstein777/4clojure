06bf4a5e9b1e68c41cc18e864b7469b59d1ebcb4
stuart <qmstuart@gmail.com>
2019-06-23 23:45:58 +0100

    Solved problem 166

--- src/four_clojure/problems_161_to_170.clj
+++ src/four_clojure/problems_161_to_170.clj
@@ -31,3 +31,21 @@
 (= 1 (if [0] 1 0))
 (= 1 (if 0 1 0))
 (= 1 (if 1 1 0))
+
+;; Problem 166
+;; Difficulty: Easy
+;; Topics
+;; For any orderable data type it's possible to derive all of the basic comparison operations
+;; (<, ≤, =, ≠, ≥, and >) from a single operation (any operator but = or ≠ will work).
+;; Write a function that takes three arguments, a less than operator for the data and two items
+;; to compare.
+;; The function should return a keyword describing the relationship between the two items.
+;; The keywords for the relationship between x and y are as follows:
+;; x = y -> :eq
+;; x > y -> :gt
+;; x < y -> :lt
+; (= :gt (__ < 5 1))
+; (= :eq (__ (fn [x y] (< (count x) (count y))) "pear" "plum"))
+; (= :lt (__ (fn [x y] (< (mod x 5) (mod y 5))) 21 3))
+; (= :gt (__ > 0 2))
+#(cond (% %2 %3) :lt (% %3 %2) :gt :else :eq)
