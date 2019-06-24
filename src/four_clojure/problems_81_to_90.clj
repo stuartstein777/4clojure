0e342e09dde9e10b522535f6b8992e2dc0249a40
stuart <qmstuart@gmail.com>
2019-06-24 22:51:06 +0100

    Solved problem 90

--- src/four_clojure/problems_81_to_90.clj
+++ src/four_clojure/problems_81_to_90.clj
@@ -44,3 +44,18 @@
 ; (= (__ #{[1 2] [2 3]} #{[2 3] [3 4]}) #{[1 2] [3 4]})
 #(clojure.set/union (clojure.set/difference % %2) (clojure.set/difference %2 %))
 
+;; Problem 90: Cartesian Product
+;; Difficulty: Easy
+;; Topics: Set Theory
+;; Write a function which calculates the Cartesian product of two sets.
+; (= (__ #{"ace" "king" "queen"} #{"♠" "♥" "♦" "♣"})
+;   #{["ace"   "♠"] ["ace"   "♥"] ["ace"   "♦"] ["ace"   "♣"]
+;     ["king"  "♠"] ["king"  "♥"] ["king"  "♦"] ["king"  "♣"]
+;     ["queen" "♠"] ["queen" "♥"] ["queen" "♦"] ["queen" "♣"]})
+;
+; (= (__ #{1 2 3} #{4 5})
+;   #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})
+;
+; (= 300 (count (__ (into #{} (range 10))
+;                  (into #{} (range 30)))))
+#(set (for [x % y %2] [x y]))
