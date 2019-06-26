4ec0d3c7dec339dc85837de3aaef252afaf3f89a
stuart <qmstuart@gmail.com>
2019-06-26 22:32:02 +0100

    Solved problem 99 and 107

---
+++ src/four_clojure/problems_91_to_100.clj
@@ -0,0 +1,14 @@
+(ns four-clojure.problems-91-to-100)
+
+;; Problem 99: product Digits
+;; Difficulty: Easy
+;; Topics: math seqs
+;; Write a function which multiplies two numbers and returns the result as a sequence of its digits.
+; (= (__ 1 1) [1])
+; (= (__ 99 9) [8 9 1])
+; (= (__ 999 99) [9 8 9 0 1])
+(fn seq-prod [a b]
+  ((fn seq-prod-iter [x c]
+     (if (pos? x)
+       (seq-prod-iter (/ (- x (rem x 10)) 10) (conj c (rem x 10)))
+       (reverse c))) (* a b) []))
