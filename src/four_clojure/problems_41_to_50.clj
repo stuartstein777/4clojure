8d2bb71507f78f42d452fc4b0d33f051ad49e97d
Stuart Stein <qmstuart@gmail.com>
2021-07-17 01:27:17 +0100

    added problem 50

--- src/four_clojure/problems_41_to_50.clj
+++ src/four_clojure/problems_41_to_50.clj
@@ -98,3 +98,14 @@
 ; (= (__ 2 [[1 2] [3 4] [5 6]]) [[[1 2] [3 4]] [[5 6]]])
 ; Restrictions: DO NOT USE split-at
 #(conj [] (take % %2) (drop % %2))
+
+;; Problem 50: Split on type
+;; Difficulty: Medium
+;; Write a function which takes a sequence consisting of items with different types and splits
+;; them up into a set of homogeneous sub-sequences.
+;; The internal order of each sub-sequence should be maintained, but the sub-sequences themselves
+;; can be returned in any order (this is why 'set' is used in the test cases).
+; (= (set (__ [1 :a 2 :b 3 :c])) #{[1 2 3] [:a :b :c]})
+; (= (set (__ [:a "foo"  "bar" :b])) #{[:a :b] ["foo" "bar"]})
+; (= (set (__ [[1 2] :a [3 4] 5 6 :b])) #{[[1 2] [3 4]] [:a :b] [5 6]})
+#(map second (group-by type %))
