b5ab2ab37fb6fbfed4246cbd4b770a218b27dc74
Stuart Stein <qmstuart@gmail.com>
2021-07-17 01:20:35 +0100

    Added problem 46 solution

--- src/four_clojure/problems_41_to_50.clj
+++ src/four_clojure/problems_41_to_50.clj
@@ -59,6 +59,14 @@
 ; (= __ (take 5 (iterate #(+ 3 %) 1)))
 (= '(1 4 7 10 13) (take 5 (iterate #(+ 3 %) 1)))
 
+;; Problem 46: Higher Order Functions
+;; Write a higher-order function which flips the order of the arguments of an input function
+; (= 3 ((__ nth) 2 [1 2 3 4 5]))
+; (= true ((__ >) 7 8))
+; (= 4 ((__ quot) 2 8))
+; (= [1 2 3] ((__ take) [1 2 3 4 5] 3))
+(fn [f] (fn [a b] (f b a)))
+
 ;; Problem 47: Contain yourself
 ;; Difficulty: Easy
 ;; Topics:
